b159a5b947d31a4bc49636b3915e36efe5453f63
Ray Meredith <ray.meredith@gmail.com>
2017-01-22 22:35:11 -0600

    Add update script.

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/todoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to mongodb server.');
+  }
+  console.log('Connected to mongodb server.');
+
+  // db.collection('Todos').findOneAndReplace({
+  //   _id: new ObjectID('58856e7b9a3e66b529ca3050')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndReplace({
+    name: 'Will Robinson'
+  }, {
+    $set: {
+      name: 'Tony Danza'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
