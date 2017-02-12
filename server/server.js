c7b27bb63ef868bef83f069b94fdb0c0d4eca690
Ray Meredith <ray.meredith@gmail.com>
2017-02-12 17:02:26 -0600

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -96,6 +97,15 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
+app.get('/users', (req, res) => {
+  User.find().then((users) => {
+    res.send({users});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+// POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
@@ -109,13 +119,10 @@ app.post('/users', (req, res) => {
   })
 });
 
-app.get('/users', (req, res) => {
-  Todo.find().then((users) => {
-    res.send({users});
-  }, (e) => {
-    res.status(400).send(e);
-  });
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
+
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
